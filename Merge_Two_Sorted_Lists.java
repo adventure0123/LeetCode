b1b7501306ed21fd064e60c4f41f6f013b6bf49c
adventure0123 <yuanjianwei91@gmail.com>
2015-05-19 22:10:57 +0800

    No 21

---
+++ Merge_Two_Sorted_Lists.java
@@ -0,0 +1,42 @@
+package com.LeetCode;
+
+public class Merge_Two_Sorted_Lists {
+
+	public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+		if (l1 == null) {
+			return l2;
+		}
+		if (l2 == null) {
+			return l1;
+		}
+		ListNode result = new ListNode(-1);
+		ListNode node = result;
+		while (l1 != null && l2 != null) {
+			if (l1.val < l2.val) {
+				node.next = l1;
+				l1 = l1.next;
+			} else {
+				node.next = l2;
+				l2 = l2.next;
+			}
+			node = node.next;
+		}
+		if (l1 != null) {
+			node.next = l1;
+		}
+		if (l2 != null) {
+			node.next = l2;
+		}
+		return result.next;
+
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
