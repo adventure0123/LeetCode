716752e69f1491d691583c1df4a293dea8c15a5d
adventure0123 <yuanjianwei91@gmail.com>
2015-05-20 17:20:24 +0800

    No 27

---
+++ Remove_Element.java
@@ -0,0 +1,24 @@
+package com.LeetCode;
+
+/**
+ * Created by yuanjianwei on 2015/5/20.
+ */
+public class Remove_Element {
+    public int removeElement(int[] nums, int val) {
+        int length = nums.length;
+        if (length == 0) {
+            return 0;
+        }
+        int index = 0;
+        for (int i = 0; i < length; i++) {
+            if (val != nums[i]) {
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+        return index;
+    }
+    public static void main(String[] args) {
+
+    }
+}
