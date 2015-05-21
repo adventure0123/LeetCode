ad6c5e6c881db9c0adb46082a48d3e5e22cb822d
adventure0123 <yuanjianwei91@gmail.com>
2015-05-21 14:51:46 +0800

    No 55

---
+++ Jump_Game.java
@@ -0,0 +1,34 @@
+package com.LeetCode;
+
+/**
+ * Created by yuanjianwei on 2015/5/21.
+ */
+public class Jump_Game {
+    public boolean canJump(int[] nums) {
+        int length = nums.length;
+        if (length == 0 || length == 1) {
+            return true;
+        }
+        int index = 0;
+        for (int i = 0; i < length; i++) {
+            if (index >= length - 1) {
+                return true;
+            } else {
+                if (i + nums[i] > index) {
+                    index = i + nums[i];
+                } else {
+                    if (index == i) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Jump_Game test = new Jump_Game();
+        int[] nums = {3, 2, 1, 0, 4};
+        System.out.println(test.canJump(nums));
+    }
+}
