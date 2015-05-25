d24f5304f9ef42fda5afc915b9d5bca5c1c7815f
adventure0123 <yuanjianwei91@gmail.com>
2015-05-25 12:58:07 +0800

    No 217

---
+++ Contains_Duplicate.java
@@ -0,0 +1,27 @@
+package com.LeetCode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by yuanjianwei on 2015/5/25.
+ */
+public class Contains_Duplicate {
+    public boolean containsDuplicate(int[] nums) {
+        int length=nums.length;
+        if (length==0){
+            return false;
+        }
+        Map<Integer,Boolean> map=new HashMap<>();
+        for(int i=0;i<length;i++){
+            if(!map.containsKey(nums[i])){
+                map.put(nums[i],true);
+            }else{
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+}
