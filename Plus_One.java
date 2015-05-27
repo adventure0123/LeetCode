e16c05468f8cbf8ee0efcb6ad47b589677b1f7b8
adventure0123 <yuanjianwei91@gmail.com>
2015-05-27 17:42:45 +0800

    No 66

---
+++ Plus_One.java
@@ -0,0 +1,28 @@
+package com.LeetCode;
+
+/**
+ * Created by yuanjianwei on 2015/5/27.
+ */
+public class Plus_One {
+    public int[] plusOne(int[] digits) {
+        int length=digits.length;
+        int carry_bit=1;
+        int temp=0;
+        for(int i=length-1;i>=0;i--){
+            temp=carry_bit+digits[i];
+            if(temp<10){
+                digits[i]=temp;
+                return digits;
+            }else{
+                carry_bit=1;
+                digits[i]=temp-10;
+            }
+        }
+        int[] result=new int[length+1];
+        result[0]=1;
+        for(int i=1;i<=length;i++){
+            result[i]=digits[i-1];
+        }
+        return result;
+    }
+}
