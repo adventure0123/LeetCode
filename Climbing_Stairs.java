7e7955ac26896c39fcd2b0d4971a0afdc8e3207d
adventure0123 <yuanjianwei91@gmail.com>
2015-05-28 11:34:18 +0800

    No 70

---
+++ Climbing_Stairs.java
@@ -0,0 +1,25 @@
+package com.LeetCode;
+
+/**
+ * Created by yuanjianwei on 2015/5/28.
+ */
+public class Climbing_Stairs {
+    public int climbStairs(int n) {
+        if(n==0){
+            return 0;
+        }
+        if(n==1){
+            return 1;
+        }
+        if(n==2){
+            return 2;
+        }
+       int[] A=new int[n];
+        A[0]=1;
+        A[1]=2;
+        for(int i=2;i<n;i++){
+            A[i]=A[i-1]+A[i-2];
+        }
+        return A[n-1];
+    }
+}
