439ff9b6a87f72cf224defe0dcc664da7f80d75b
adventure0123 <yuanjianwei91@gmail.com>
2015-05-29 15:43:06 +0800

    No 219

---
+++ Contains_Duplicate_II.java
@@ -0,0 +1,35 @@
+package com.LeetCode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Contains_Duplicate_II {
+
+	public boolean containsNearbyDuplicate(int[] nums, int k) {
+		int length=nums.length;
+		if(length==0){
+			return false;
+		}
+		Map<Integer, Integer> map=new HashMap<Integer, Integer>();
+		for(int i=0;i<length;i++){
+			if(map.containsKey(nums[i])){
+				int temp=map.get(nums[i]);
+				if(i-temp<=k){
+					return true;
+				}
+			}else {
+				map.put(nums[i], i);
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
