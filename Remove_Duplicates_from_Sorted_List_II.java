90eefade5f0cb6e5adbe7fab6631534298cd07e0
adventure0123 <yuanjianwei91@gmail.com>
2015-06-01 15:50:54 +0800

    No 82

---
+++ Remove_Duplicates_from_Sorted_List_II.java
@@ -0,0 +1,31 @@
+package com.LeetCode;
+
+/**
+ * Created by yuanjianwei on 2015/6/1.
+ */
+public class Remove_Duplicates_from_Sorted_List_II {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        ListNode result = new ListNode(-1);
+        result.next = head;
+        ListNode node = result;
+        int count;
+        while (head != null) {
+            count = 1;
+            while (head.next != null && head.val == head.next.val) {
+                count++;
+                head = head.next;
+            }
+            if (count == 1) {
+                node.next = head;
+                node = head;
+            } else {
+                node.next = head.next;
+            }
+            head = head.next;
+        }
+        return result.next;
+    }
+}
