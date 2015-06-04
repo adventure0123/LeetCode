2ed2a659429238461d7913e152e296ccd94e17e5
adventure0123 <yuanjianwei91@gmail.com>
2015-06-04 21:36:29 +0800

    No 220

---
+++ Contains_Duplicate_III.java
@@ -0,0 +1,37 @@
+package com.LeetCode;
+
+import java.util.TreeSet;
+
+public class Contains_Duplicate_III {
+	public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+		int length = nums.length;
+		if (length == 0 || k <= 0) {
+			return false;
+		}
+		TreeSet<Integer> treeSet = new TreeSet<>();
+		Integer up, down;
+		for (int i = 0; i < length; i++) {
+			up = treeSet.floor(nums[i] + t);
+			down = treeSet.ceiling(nums[i] - t);
+			if ((up != null && up >= nums[i])
+					|| (down != null || down <= nums[i])) {
+				return true;
+			}
+			treeSet.add(nums[i]);
+			if (i >= k) {
+				treeSet.remove(nums[i - k]);
+			}
+		}
+		return false;
+
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
