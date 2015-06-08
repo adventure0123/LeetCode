e88a0e757d9cc6cce7eb42ff63d281ba247b6d97
adventure0123 <yuanjianwei91@gmail.com>
2015-06-08 21:42:44 +0800

    No 223

---
+++ Rectangle_Area.java
@@ -0,0 +1,26 @@
+package com.LeetCode;
+
+public class Rectangle_Area {
+	 public int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
+		 int x=Math.max(A, E);
+		 int y=Math.min(C, G);
+		 int z=Math.max(B, F);
+		 int w=Math.min(D, H);
+		 int total=(C-A)*(D-B)+(G-E)*(H-F);
+		 if(x>=y||z>=w){
+			 return total;
+		 }else{
+			 return total-(y-x)*(w-z);
+		 }
+		 
+	        
+	    }
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
