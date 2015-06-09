6e83d2b7057c08ff42d1c7082556647cd7488e71
adventure0123 <yuanjianwei91@gmail.com>
2015-06-09 22:47:44 +0800

    No 94

---
+++ Binary_Tree_Inorder_Traversal.java
@@ -0,0 +1,30 @@
+package com.LeetCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Binary_Tree_Inorder_Traversal {
+	public List<Integer> inorderTraversal(TreeNode root) {
+		List<Integer> result = new ArrayList<>();
+		if (root == null) {
+			return result;
+		}
+		addNode(result, root);
+		return result;
+
+	}
+
+	private void addNode(List<Integer> result, TreeNode root) {
+		if (root.left != null) {
+			addNode(result, root.left);
+		}
+		result.add(root.val);
+		if (root.right != null) {
+			addNode(result, root.right);
+		}
+	}
+
+	public static void main(String[] args) {
+
+	}
+}
