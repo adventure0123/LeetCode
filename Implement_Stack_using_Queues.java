4a7d5a0f18a6eff46b1ac7046b7ca4f825b05cf4
adventure0123 <yuanjianwei91@gmail.com>
2015-06-11 22:22:00 +0800

    No 225

---
+++ Implement_Stack_using_Queues.java
@@ -0,0 +1,32 @@
+package com.LeetCode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Implement_Stack_using_Queues {
+	Queue<Integer> queue = new LinkedList<>();
+
+	// Push element x onto stack.
+	public void push(int x) {
+		queue.add(x);
+		int size = queue.size();
+		for (int i = 1; i < size; i++) {
+			queue.add(queue.poll());
+		}
+	}
+
+	// Removes the element on top of the stack.
+	public void pop() {
+		queue.poll();
+	}
+
+	// Get the top element.
+	public int top() {
+		return queue.peek();
+	}
+
+	// Return whether the stack is empty.
+	public boolean empty() {
+		return queue.isEmpty();
+	}
+}
