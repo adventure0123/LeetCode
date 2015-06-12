8cb3810415c3a3a21383b867c0acaedace40b937
adventure0123 <yuanjianwei91@gmail.com>
2015-06-12 22:56:31 +0800

    No 110

---
+++ Balanced_Binary_Tree.java
@@ -0,0 +1,35 @@
+package com.LeetCode;
+
+public class Balanced_Binary_Tree {
+	public boolean isBalanced(TreeNode root) {
+		if (root == null) {
+			return true;
+		}
+		int left = balance(root.left);
+		int right = balance(root.right);
+		if (left == -1 || right == -1) {
+			return false;
+		}
+		if (Math.abs(left - right) > 1) {
+			return false;
+		} else {
+			return true;
+		}
+
+	}
+
+	private int balance(TreeNode root) {
+		if (root == null) {
+			return 0;
+		}
+		int left = balance(root.left);
+		int right = balance(root.right);
+		if (left == -1 || right == -1) {
+			return -1;
+		}
+		if (Math.abs(left - right) > 1) {
+			return -1;
+		}
+		return left > right ? left + 1 : right + 1;
+	}
+}
