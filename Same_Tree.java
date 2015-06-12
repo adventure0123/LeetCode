90276a142fd53cf0f1f52c1a1dff9ab194d1e771
adventure0123 <yuanjianwei91@gmail.com>
2015-06-12 17:25:56 +0800

    No 100

---
+++ Same_Tree.java
@@ -0,0 +1,19 @@
+package com.LeetCode;
+
+/**
+ * Created by yuanjianwei on 2015/6/12.
+ */
+public class Same_Tree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if ((p == null && q != null) || (p != null && q == null)) {
+            return false;
+        }
+        if (p.val != q.val) {
+            return false;
+        }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
