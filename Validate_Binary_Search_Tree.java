0cdf2b179eddb5afdcd1cd75e0cb1dc9583715a7
adventure0123 <yuanjianwei91@gmail.com>
2015-06-12 17:21:48 +0800

    No 98

---
+++ Validate_Binary_Search_Tree.java
@@ -0,0 +1,28 @@
+package com.LeetCode;
+
+/**
+ * Created by yuanjianwei on 2015/6/12.
+ */
+public class Validate_Binary_Search_Tree {
+    public boolean isValidBST(TreeNode root) {
+        return isBST(root, Integer.MIN_VALUE - 1L, Integer.MAX_VALUE + 1L);
+    }
+
+    private boolean isBST(TreeNode root, long min, long max) {
+        System.out.println(min);
+        if (root == null) {
+            return true;
+        }
+        if (root.val <= min || root.val >= max) {
+            return false;
+        }
+        return isBST(root.left, min, root.val) && isBST(root.right, root.val, max);
+
+    }
+
+    public static void main(String[] args) {
+        Validate_Binary_Search_Tree test = new Validate_Binary_Search_Tree();
+        TreeNode root = new TreeNode(Integer.MAX_VALUE);
+        System.out.println(test.isValidBST(root));
+    }
+}
