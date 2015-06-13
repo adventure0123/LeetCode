562de5c5c43cfc9ae4ab0e13e779383773fe258a
adventure0123 <yuanjianwei91@gmail.com>
2015-06-13 21:23:31 +0800

    No 111

---
+++ Minimum_Depth_of_Binary_Tree.java
@@ -0,0 +1,18 @@
+package com.LeetCode;
+
+public class Minimum_Depth_of_Binary_Tree {
+	public int minDepth(TreeNode root) {
+		if (root == null) {
+			return 0;
+		}
+		int left = minDepth(root.left);
+		int right = minDepth(root.right);
+		if (left == 0 && right != 0) {
+			return right + 1;
+		}
+		if (right == 0 && left != 0) {
+			return left + 1;
+		}
+		return left < right ? left + 1 : right + 1;
+	}
+}
