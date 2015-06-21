6d5900044cc24374d1fb9041b32d06d046c2f25d
adventure0123 <yuanjianwei91@gmail.com>
2015-06-21 21:21:36 +0800

    No 137

---
+++ Single_Number_II.java
@@ -0,0 +1,29 @@
+package com.LeetCode;
+
+import java.util.Arrays;
+
+public class Single_Number_II {
+	public int singleNumber(int[] nums) {
+		int length = nums.length;
+		int[] A = new int[32];
+		int temp;
+		for (int i = 0; i < length; i++) {
+			temp = nums[i];
+			for (int j = 31; j >= 0; j--) {
+				A[j] += (temp >> j) & 1;
+			}
+		}
+		int result = 0;
+		for (int i = 31; i >= 0; i--) {
+			result = (result << 1) + A[i] % 3;
+		}
+		return result;
+
+	}
+
+	public static void main(String[] args) {
+		Single_Number_II test = new Single_Number_II();
+		int[] A = { 2, 1, 1, 1 };
+		System.out.println(test.singleNumber(A));
+	}
+}
