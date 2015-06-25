50a8ea50b2ae7436a3ef686507785aaf7d1e58d3
adventure0123 <yuanjianwei91@gmail.com>
2015-06-25 14:56:27 +0800

    No 142

---
+++ Linked_List_Cycle_II.java
@@ -0,0 +1,27 @@
+package com.LeetCode;
+
+/**
+ * Created by yuanjianwei on 2015/6/25.
+ */
+public class Linked_List_Cycle_II {
+    public ListNode detectCycle(ListNode head) {
+        if(head==null){
+            return null;
+        }
+        ListNode fast=head;
+        ListNode slow=head;
+        while(fast!=null&&fast.next!=null){
+            fast=fast.next.next;
+            slow=slow.next;
+            if(fast==slow){
+                fast=head;
+                while(fast!=slow){
+                    fast=fast.next;
+                    slow=slow.next;
+                }
+                return fast;
+            }
+        }
+        return null;
+    }
+}
