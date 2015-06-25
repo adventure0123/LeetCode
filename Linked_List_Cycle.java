1c025b3d703fdf40f2219065e39a34b40f9545a5
adventure0123 <yuanjianwei91@gmail.com>
2015-06-25 14:56:19 +0800

    No 141

---
+++ Linked_List_Cycle.java
@@ -0,0 +1,22 @@
+package com.LeetCode;
+
+/**
+ * Created by yuanjianwei on 2015/6/25.
+ */
+public class Linked_List_Cycle {
+    public boolean hasCycle(ListNode head) {
+        if(head==null){
+            return false;
+        }
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null&&fast.next!=null){
+            fast=fast.next.next;
+            slow=slow.next;
+            if(fast==slow){
+                return true;
+            }
+        }
+        return false;
+    }
+}
