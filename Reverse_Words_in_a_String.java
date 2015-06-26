161ba1df5869c9372d777aed026f321e13b4f05c
adventure0123 <yuanjianwei91@gmail.com>
2015-06-26 17:15:54 +0800

    No 151

---
+++ Reverse_Words_in_a_String.java
@@ -0,0 +1,26 @@
+package com.LeetCode;
+
+public class Reverse_Words_in_a_String {
+	public String reverseWords(String s) {
+
+		if (s.length() == 0) {
+			return "";
+		}
+		s = s.trim();
+		String[] str = s.split(" ");
+		StringBuffer bf = new StringBuffer();
+		int size = str.length;
+		for (int i = size - 1; i >= 0; i--) {
+			if (!str[i].equals("")) {
+				if (i == size - 1) {
+					bf.append(str[i]);
+				} else {
+					bf.append(" ");
+					bf.append(str[i]);
+				}
+			}
+		}
+		return bf.toString();
+
+	}
+}
