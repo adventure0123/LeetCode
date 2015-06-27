68ae368180a5889e2859825072b53ed85b9b38e0
adventure0123 <yuanjianwei91@gmail.com>
2015-06-27 22:34:18 +0800

    No 155

---
+++ Min_Stack.java
@@ -0,0 +1,35 @@
+package com.LeetCode;
+
+import java.util.Stack;
+
+public class Min_Stack {
+	Stack<Integer> stack=new Stack<>();
+	Stack<Integer> minStack=new Stack<>();
+	 public void push(int x) {
+	        stack.push(x);
+	        if(minStack.isEmpty()){
+	        	minStack.push(x);
+	        	return;
+	        }
+	        if(x<=minStack.peek()){
+	        	minStack.push(x);
+	        }
+	    }
+
+	    public void pop() {
+	        int x=stack.pop();
+	        if(x==minStack.peek()){
+	        	minStack.pop();
+	        }
+	    }
+
+	    public int top() {
+			return stack.peek();
+	        
+	    }
+
+	    public int getMin() {
+			return minStack.peek();
+	        
+	    }
+}
