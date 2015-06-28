455a34cf592748295a64dbd3c28b9b2cfb1b983c
adventure0123 <yuanjianwei91@gmail.com>
2015-06-28 17:22:56 +0800

    No 172

---
+++ Factorial_Trailing_Zeroes.java
@@ -0,0 +1,12 @@
+package com.LeetCode;
+
+public class Factorial_Trailing_Zeroes {
+	public int trailingZeroes(int n) {
+		if(n<5){
+			return 0;
+		}else{
+			return trailingZeroes(n/5)+n/5;
+		}
+        
+    }
+}
