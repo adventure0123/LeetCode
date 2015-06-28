1bcecafbdb184a3fe10729c4598803b58350ad71
adventure0123 <yuanjianwei91@gmail.com>
2015-06-28 17:05:57 +0800

    No 171

---
+++ Excel_Sheet_Column_Number.java
@@ -0,0 +1,13 @@
+package com.LeetCode;
+
+public class Excel_Sheet_Column_Number {
+	public int titleToNumber(String s) {
+		int length = s.length();
+		int result = 0;
+		for (int i = 0; i < length; i++) {
+			result = result * 26 + (s.charAt(i) - 'A') + 1;
+		}
+		return result;
+
+	}
+}
