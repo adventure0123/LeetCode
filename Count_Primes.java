53c2ab692d4b7c33fc6c1bcfb086e030b7a199bf
adventure0123 <yuanjianwei91@gmail.com>
2015-07-01 15:50:12 +0800

    No 204

---
+++ Count_Primes.java
@@ -0,0 +1,28 @@
+package com.LeetCode;
+
+/**
+ * Created by yuanjianwei on 2015/7/1.
+ */
+public class Count_Primes {
+    public int countPrimes(int n) {
+        if(n<2){
+            return 0;
+        }
+        boolean[] A=new boolean[n];
+        int temp;
+        for(int i=2;i<Math.sqrt(n);i++){
+            if(!A[i]) {
+                for (int j = i; i * j < n; j++) {
+                    A[i*j]=true;
+                }
+            }
+        }
+        int sum=0;
+        for(int i=2;i<n;i++){
+            if(!A[i]){
+                sum++;
+            }
+        }
+        return sum;
+    }
+}
