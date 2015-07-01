3696e4bf9aa7507fa03d82b230fe6a5cc08b887a
adventure0123 <yuanjianwei91@gmail.com>
2015-07-01 15:17:58 +0800

    No 202

---
+++ Happy_Number.java
@@ -0,0 +1,37 @@
+package com.LeetCode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by yuanjianwei on 2015/7/1.
+ */
+public class Happy_Number {
+    public boolean isHappy(int n) {
+        Map<Integer,Boolean> map=new HashMap<>();
+        map.put(n,true);
+        int sum=0,bit;
+        while(n!=1){
+            sum=0;
+           while(n!=0){
+               bit=n%10;
+               n=n/10;
+               sum=sum+bit*bit;
+           }
+            if(sum==1){
+                return true;
+            }else if(map.containsKey(sum)){
+                return false;
+            }else{
+                map.put(sum,true);
+                n=sum;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Happy_Number test=new Happy_Number();
+        System.out.println(test.isHappy(19));
+    }
+}
