dff560e12ee476cf0efc63b58df868c234e650cd
adventure0123 <yuanjianwei91@gmail.com>
2015-07-08 14:04:01 +0800

    No 231

---
+++ Power_of_Two.java
@@ -0,0 +1,30 @@
+package com.LeetCode;
+
+/**
+ * Created by yuanjianwei on 2015/7/6.
+ */
+public class Power_of_Two {
+    public boolean isPowerOfTwo(int n) {
+        if(n==1){
+            return true;
+        }
+        if(n>0) {
+            int temp = 2;
+            while (temp <= n) {
+                if (temp == n) {
+                    return true;
+                }
+                if (temp >= (Integer.MAX_VALUE >> 1)) {
+                    return false;
+                }
+                temp = temp << 1;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Power_of_Two test=new Power_of_Two();
+        System.out.println(test.isPowerOfTwo(-8));
+    }
+}
