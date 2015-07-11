2557bb9a6c28299effe33c8ca0f554e6dab06e70
adventure0123 <yuanjianwei91@gmail.com>
2015-07-11 16:40:54 +0800

    No 133

---
+++ UndirectedGraphNode.java
@@ -0,0 +1,14 @@
+package com.LeetCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UndirectedGraphNode {
+	int label;
+	List<UndirectedGraphNode> neighbors;
+
+	UndirectedGraphNode(int x) {
+		label = x;
+		neighbors = new ArrayList<UndirectedGraphNode>();
+	}
+}
