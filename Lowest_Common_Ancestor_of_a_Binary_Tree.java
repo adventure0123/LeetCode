d99bab97c99e1e83cce7d4c224887477707fa187
adventure0123 <yuanjianwei91@gmail.com>
2015-07-13 22:38:00 +0800

    No 236

---
+++ Lowest_Common_Ancestor_of_a_Binary_Tree.java
@@ -0,0 +1,27 @@
+package com.LeetCode;
+
+import java.util.Stack;
+
+public class Lowest_Common_Ancestor_of_a_Binary_Tree {
+	public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+		if(root==null||q==null||p==null){
+			return null;
+		}
+		if(root==p||root==q){
+			return root;
+		}
+		TreeNode left=lowestCommonAncestor(root.left, p, q);
+		TreeNode right=lowestCommonAncestor(root.right, p, q);
+		
+		return left==null?right:right==null?left:root;
+        
+    }
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
