9dc4990e0c8a36d9dfb9c35a233aec68e2da63e0
adventure0123 <yuanjianwei91@gmail.com>
2015-07-16 22:11:49 +0800

    No 237

---
+++ Delete_Node_in_a_Linked_List.java
@@ -0,0 +1,15 @@
+package com.LeetCode;
+
+public class Delete_Node_in_a_Linked_List {
+	public void deleteNode(ListNode node) {
+        if(node==null){
+        	return;
+        }else if(node.next==null){
+        	node=null;
+        }else {
+        	node.val=node.next.val;
+        	node.next=node.next.next;
+        }
+    }
+
+}
