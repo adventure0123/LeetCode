0b74bfde144c25d1b328406f6da2266f337bcb26
adventure0123 <yuanjianwei91@gmail.com>
2015-08-17 22:49:46 +0800

    No 258

---
+++ Add_Digits.java
@@ -0,0 +1,16 @@
+package com.LeetCode;
+
+public class Add_Digits {
+	public int addDigits(int num) {
+		return (num-1)%9+1;
+        
+    }
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
