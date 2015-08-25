c271231e567655fd03483a50cee87009a081d9fe
adventure0123 <yuanjianwei91@gmail.com>
2015-08-25 22:13:28 +0800

    No 268

---
+++ Missing_Number.java
@@ -0,0 +1,41 @@
+package com.LeetCode;
+
+public class Missing_Number {
+	public int missingNumber(int[] nums) {
+		int length = nums.length;
+		int temp = 0;
+		int addition = -1;
+		for (int i = 0; i < length; i++) {
+			if (nums[i] == i || nums[i] == addition) {
+				continue;
+			} else {
+				if (nums[i] >= length) {
+					nums[i] = addition;
+					continue;
+				}
+				temp = nums[nums[i]];
+				nums[nums[i]] = nums[i];
+				nums[i] = temp;
+				i--;
+			}
+		}
+		for (int i = 0; i < length; i++) {
+			if (nums[i] == addition) {
+				return i;
+			}
+		}
+		return length;
+
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Missing_Number test = new Missing_Number();
+		int[] A = { 0, 1, 3, 2 };
+		System.out.println(test.missingNumber(A));
+	}
+
+}
