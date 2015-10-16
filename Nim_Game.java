59d636a8038af8fd22db81c81b82bf9e547c6934
adventure0123 <yuanjianwei91@gmail.com>
2015-10-16 16:13:36 +0800

    No 292

---
+++ Nim_Game.java
@@ -0,0 +1,19 @@
+package com.LeetCode;
+
+public class Nim_Game {
+	public boolean canWinNim(int n) {
+		if(n%4==0){
+			return false;
+		}
+		return true;
+        
+    }
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
