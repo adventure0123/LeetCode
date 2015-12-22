0c32f9ebb14a47b46352d11ad06111b534f4b659
adventure0123 <yuanjianwei91@gmail.com>
2015-12-22 16:47:52 +0800

    No 319

---
+++ Bulb_Switcher.java
@@ -0,0 +1,19 @@
+package com.LeetCode;
+
+import java.util.Arrays;
+
+public class Bulb_Switcher {
+	public int bulbSwitch(int n) {
+		return (int)Math.sqrt(n);
+        
+    }
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Bulb_Switcher test=new Bulb_Switcher();
+		System.out.println(test.bulbSwitch(3));
+	}
+
+}
