3c321ff6dbe1f6ffaef57f211186aa091fe3c016
adventure0123 <yuanjianwei91@gmail.com>
2016-03-16 17:07:43 +0800

    No 337

--- House_Robber_III.java
+++ House_Robber_III.java
@@ -13,21 +13,21 @@ public class House_Robber_III {
 		if(root==null){
 			return 0;
 		}
-		return 0;
+		return noNode(root.left)+noNode(root.right)+root.val;
 	}
 	
 	public int noNode(TreeNode root){
 		if(root==null){
 			return 0;
 		}
-		return 0;
+		return rob(root.left)+rob(root.right);
 	}
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 }
