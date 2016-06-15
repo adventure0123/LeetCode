4d6ecb210190ffd98509f776817c46499a84616a
adventure <yuanjianwei91@gmail.com>
2016-06-15 23:36:39 +0800

    No 357

---
+++ Count_Numbers_with_unique_Digits.java
@@ -0,0 +1,24 @@
+/**
+ * Created by adventure on 16/6/15.
+ */
+public class Count_Numbers_with_unique_Digits {
+
+    public int countNumberWithUniqueDigits(int n){
+        if(n==0){
+            return 1;
+        }
+        if(n==1){
+            return 10;
+        }
+        if(n>=10){
+            return  countNumberWithUniqueDigits(10);
+        }
+        int temp=countNumberWithUniqueDigits(n-1);
+        return (temp-1)*(10-n+1)+temp;
+    }
+
+    public static void main(String[] args) {
+        Count_Numbers_with_unique_Digits test=new Count_Numbers_with_unique_Digits();
+        System.out.println(test.countNumberWithUniqueDigits(3));
+    }
+}
